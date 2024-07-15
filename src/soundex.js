db88cd71c3699e3ca7cecd47aa1657e85b1abee0
revathiVinoth <101211458+revathiVinoth@users.noreply.github.com>
2024-07-15 10:41:40 +0530

    Update soundex.js

--- src/soundex.js
+++ src/soundex.js
@@ -1,6 +1,4 @@
-function getSoundexCode(char) {
-    char = char.toUpperCase();
-    const soundexDict = {
+const soundexDict = {
         'B': '1', 'F': '1', 'P': '1', 'V': '1',
         'C': '2', 'G': '2', 'J': '2', 'K': '2', 'Q': '2', 'S': '2', 'X': '2', 'Z': '2',
         'D': '3', 'T': '3',
@@ -8,6 +6,9 @@ function getSoundexCode(char) {
         'M': '5', 'N': '5',
         'R': '6'
     };
+
+function getSoundexCode(char) {
+    char = char.toUpperCase();
     return soundexDict[char] || '0';
 }
 
